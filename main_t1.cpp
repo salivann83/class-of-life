0bb19edcf5096eb83e5c3e8ec164e867e58f9860
Ehsan pichka <ehsanpichka1@gmail.com>
2023-04-01 21:57:32 +0430

    start of project

---
+++ main_t1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+
+
+class genome
+{
+    private:
+
+
+
+    public:
+    void get_gen();//daryaft mohtavaye gene
+    void make_dna();//sakht dna az rna
+    void small_jahesh(string a ,string b ,string c);//jahesh koochak
+    void big_jahesh(string a , string b);//jaheshe bozorg
+    void reverse_jahesh(string a);//jaheshe makoos
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    
+}
