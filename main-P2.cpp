09172f4b8d59054c459485e237fad8551f571fa3
Ehsan pichka <ehsanpichka1@gmail.com>
2023-04-05 16:28:29 +0430

    start of cell

--- main-P2.cpp
+++ main-P2.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 
-
+class cell;
 class genome
 {
     private:
@@ -14,7 +14,7 @@ class genome
     string DNA_p1;
     string DNA_p2;
     string DNA_RNA;
-
+    friend class cell;
 
 
     public:
@@ -28,7 +28,7 @@ class genome
         mokamel.insert(pair<char , char > ('A','T'));
         mokamel.insert(pair<char , char > ('T','A'));
         mokamel.insert(pair<char , char > ('C','G'));
-         mokamel.insert(pair<char , char > ('G','C'));
+        mokamel.insert(pair<char , char > ('G','C'));
     }
     void get_gen()//daryaft mohtavaye gene
     {
@@ -280,6 +280,94 @@ class genome
 
 
 };
+
+
+class cell
+{
+    private:
+    int n_chro;
+    vector<string>DNA_p1_chro;
+    vector<string>DNA_p2_chro;
+    friend class genome;
+    
+    
+    
+    public:
+    
+    map<char, char> mokamel;
+    cell()
+    {
+        mokamel.insert(pair<char , char > ('A','T'));
+        mokamel.insert(pair<char , char > ('T','A'));
+        mokamel.insert(pair<char , char > ('C','G'));
+        mokamel.insert(pair<char , char > ('G','C'));
+    }
+    
+    void get_gen_chro()
+    {
+        string DNA_p1;
+        string DNA_p2;
+
+        cout<<"ENTER  NUMBER OF chromosome: ";
+        cin>>n_chro;
+        for (int i = 0 ; i<n_chro ; i++)
+        {
+            cout<<"DNA of of "<<i+1<<"chromosome";
+            cout<<"ENTER DNA_p1 OF CHRO"<<endl;
+            cin>>DNA_p1;
+            DNA_p1_chro.push_back(DNA_p1);
+            cout<<"ENTER DNA_p2 OF CHRO";
+            cin>>DNA_p2;
+            DNA_p2_chro.push_back(DNA_p2);
+        } 
+    }
+
+
+    void death_of_cell()
+    {
+         
+        
+        string dnap1;
+        string dnap2;
+        int n_of_d =0 ;
+        for(int j = 0 ; j<n_chro ; j++)
+        {
+            dnap1 = DNA_p1_chro[j];
+            dnap2 = DNA_p2_chro[j];
+
+        
+            for(int i = 0 ; i<dnap1.length() ; i++)
+            {
+                if(mokamel[dnap1[i]]== dnap2[i])
+                {
+                    n_of_d++;
+
+                }     
+            }
+        
+
+            if(n_of_d>=5)
+            {
+                DNA_p1_chro.erase(DNA_p1_chro.begin()+j);
+                DNA_p2_chro.erase(DNA_p2_chro.begin()+j);
+                n_chro = n_chro+1;
+            }
+        
+        }
+        
+    }
+
+    void big_rejahesh();
+
+    void small_rejahesh();
+
+    void rereverse();
+
+    void pal_mok();
+
+};
+
+
 
 int main()
 {
